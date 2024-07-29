e97920c20b543a0a85833bfbdd87fbdb7e3559ac
lyang7305 <lyang7305@gmail.com~>
2024-07-29 13:03:47 -0500

    finished setting up models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'product_id'
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsTo(Tag, {
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongTo(Product, {
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
